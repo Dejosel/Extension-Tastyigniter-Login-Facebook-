301aec778ac9db686ba3298efaf57f517ae45a2a
Dejosel <nubelibreweb@gmail.com>
2017-08-29 16:33:52 -0500

    Update facebook_module.php

--- facebook_module/views/facebook_module.php
+++ facebook_module/views/facebook_module.php
@@ -1,6 +1,9 @@
-<?php
-if(!empty($authUrl)) {
-    echo '<a href="'.$authUrl.'"><img src="'.base_url().'assets/images/flogin.png" alt=""/></a>';
-}
-?>
+<div class="text-center">
+    <?php
+    if(!empty($authUrl)) {
+        echo '<a href="'.$authUrl.'" class="azm-social azm-btn azm-pill azm-shadow-bottom azm-facebook btn"><i class="fa fa-facebook"></i> '.lang('text_bottom_facebook').'</a>';}
+    ?>
+    <hr class="or">
+    <span class="span-or">or</span>
+</div>
 
